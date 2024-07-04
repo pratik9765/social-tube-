5e2e341c9a70c98edf44a8d648dea97ca25246da
pratik9765 <pratikbokde9765@gmail.com>
2024-07-04 17:41:16 +0530

    all models done

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -172,7 +172,7 @@ const logoutUser = asyncHandler(async (req, res) => {
         req.user._id,
         {
             $unset: {
-                refreshToken: 1 // this removes the field from document
+                refreshToken: null // this removes the field from document
             }
         },
         {
